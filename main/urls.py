3dc9dbb2cc5349f6d5c557dacdcb262e69d7f146
Syed Muhammad Ali Naqvi <sn07590@st.habib.edu.pk>
2022-09-12 14:31:37 +0500

    commented urls

--- main/urls.py
+++ main/urls.py
@@ -6,16 +6,16 @@ from main.utils import HashIdConverter
 register_converter(HashIdConverter, "hashid")
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout'),
-    path('delete/<int:id_>', views.delete_instance, name='delete'),
-    path('duplicate/<str:date>/<int:fyear>/<str:type>/<str:amount>/<int:cheque>/<str:namenew>/<str:name>/<str:onaccof>/<str:country>/<str:remarks>/<str:email>/<int:phone>',
-         views.duplicate, name='duplicate'),
-    path('export', views.export_csv, name='export'),
-    path('upload', views.pdf_extractor, name='upload'),
-    path('receipt/<hashid:pk>/<str:date>/<str:amount>',  # changed <int:id> to hashid:pk here and in url from index.html likewise
-         views.receipt, name='receipt'),
-    # changed <int:id> to hashid:pk here and in url from index.html likewise
-    path('records/<hashid:pk>/', views.records, name='record'),
+    #     path('', views.index, name='index'),
+    #     path('login', views.login, name='login'),
+    #     path('logout', views.logout, name='logout'),
+    #     path('delete/<int:id_>', views.delete_instance, name='delete'),
+    #     path('duplicate/<str:date>/<int:fyear>/<str:type>/<str:amount>/<int:cheque>/<str:namenew>/<str:name>/<str:onaccof>/<str:country>/<str:remarks>/<str:email>/<int:phone>',
+    #          views.duplicate, name='duplicate'),
+    #     path('export', views.export_csv, name='export'),
+    #     path('upload', views.pdf_extractor, name='upload'),
+    #     path('receipt/<hashid:pk>/<str:date>/<str:amount>',  # changed <int:id> to hashid:pk here and in url from index.html likewise
+    #          views.receipt, name='receipt'),
+    #     # changed <int:id> to hashid:pk here and in url from index.html likewise
+    #     path('records/<hashid:pk>/', views.records, name='record'),
 ]
--- set_manager/urls.py
+++ set_manager/urls.py
@@ -17,12 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-
 # importing for addings media files url
 # from django.conf import settings
 # from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', include('main.urls'))
+    # path("admin/", admin.site.urls),
+    # path('', include('main.urls'))
 ]
